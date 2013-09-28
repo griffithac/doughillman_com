02f9c6b13697c8c4245aab43bea1c9658163beef
Doug Hillman <dough.gfc@gmail.com>
2013-09-28 08:38:43 +0800

    Second Commit

--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -2,9 +2,23 @@ require 'spec_helper'
 
 describe "Tags" do
   describe "GET /tags" do
-    it "works! (now write some real specs)" do
+    it "should display some tags" do
+      @tag = Tag.create :tag => 'Quzhou'
       visit tags_path
       page.should have_content "Quzhou"
+
     end
+
+    it "can create a new tag" do
+      visit tags_path
+      fill_in 'Tag', :with => 'Shanghai'
+      click_button 'Create Tag'
+      current_path.should == tags_path
+      page.should have_content "Shanghai"
+      save_and_open_page
+
+    end
+
+
   end
 end
